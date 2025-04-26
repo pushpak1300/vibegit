8830b45e7faad62223f6e074aee8c6572e19aa55
Pushpak Chhajed <pushpak1300@gmail.com>
2025-04-26 15:32:19 +0530

    feat: enhance help command with logo display
    - Added a logo method to display a custom logo in the help output.
    - Updated `showCommandHelp` to include the logo before showing command details.
    - Refactored `formatRoot` to utilize the new logo method for improved visual appeal.

--- src/help.ts
+++ src/help.ts
@@ -1,12 +1,24 @@
-import {Help, ux} from '@oclif/core'
+import {Command, Help, ux} from '@oclif/core'
 
 export default class RootHelp extends Help {
   formatRoot(): string {
-    return ux.colorize('green',`
+    return this.logo()
+  }
+
+  public async showCommandHelp(command: Command.Loadable): Promise<void> {
+    this.log(this.logo())
+    await super.showCommandHelp(command)
+  }
+
+  private logo(): string {
+    return ux.colorize(
+      'green',
+      `
 ▗▖  ▗▖▗▄▄▄▖▗▄▄▖ ▗▄▄▄▖ ▗▄▄▖▗▄▄▄▖▗▄▄▄▖
 ▐▌  ▐▌  █  ▐▌ ▐▌▐▌   ▐▌     █    █  
 ▐▌  ▐▌  █  ▐▛▀▚▖▐▛▀▀▘▐▌▝▜▌  █    █  
  ▝▚▞▘ ▗▄█▄▖▐▙▄▞▘▐▙▄▄▖▝▚▄▞▘▗▄█▄▖  █                                  
-`);
+`,
+    )
   }
 }
